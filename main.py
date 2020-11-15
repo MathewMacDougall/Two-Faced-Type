7636b4abdd1119d52e10753612b7da8e7926298f
Mathew MacDougall <mmacd97@gmail.com>
2020-11-14 20:47:48 -0800

    basic setup for combining words

--- main.py
+++ main.py
@@ -20,22 +20,47 @@ def combine_faces(face1, face2):
 
     return common.Shape()
 
+def create_word(word1, word2):
+    assert len(word1) == len(word2)
+    combined_letters = []
+    for letter1, letter2 in zip(word1, word2):
+        face1 = FaceFactory.create_letter(letter1)
+        face2 = FaceFactory.create_letter(letter2)
+        combined_letter = combine_faces(face1, face2)
+        combined_letters.append(combined_letter)
+
+    tf = gp_Trsf()
+    p1 = ORIGIN
+    x = 0
+    new_letters = []
+    for l in combined_letters:
+        p2 = gp_Pnt(x, x, 0)
+        tf.SetTranslation(p1, p2)
+        new_letters.append(BRepBuilderAPI_Transform(l, tf).Shape())
+        x += 12
+
+    return new_letters
+
 def main():
     display, start_display, add_menu, add_function_to_menu = init_display()
     # Read keyboard input
-    letter_1 = 'I'
-    letter_2 = 'F'
+    word1 = 'IE'
+    word2 = 'FT'
+
+    letters = create_word(word1, word2)
+    for l in letters:
+        display.DisplayColoredShape(l, update=True, color="WHITE")
 
     # Create faces
-    face_1 = FaceFactory.create_letter(letter_1)
-    face_2 = FaceFactory.create_letter(letter_2)
+    # face_1 = FaceFactory.create_letter(letter_1)
+    # face_2 = FaceFactory.create_letter(letter_2)
 
     # display.DisplayShape(face_2, update=True, color="BLUE")
     # start_display()
     # return
 
     # Create combined letter shape
-    shape = combine_faces(face_1, face_2)
+    # shape = combine_faces(face_1, face_2)
 
     # Display final result
 
@@ -43,7 +68,7 @@ def main():
     display.DisplayShape(make_edge(LINE_Y), update=True, color="GREEN")
     display.DisplayShape(make_edge(LINE_Z), update=True, color="BLUE")
 
-    display.DisplayColoredShape(shape, update=True, color="WHITE")
+    # display.DisplayColoredShape(shape, update=True, color="WHITE")
 
     start_display()
 
