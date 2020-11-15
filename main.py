4b98b1645b63db87b65bf485a494990670ad7fee
Mathew MacDougall <mmacd97@gmail.com>
2020-11-14 16:01:33 -0800

    Figured out basic planes and shapes

--- main.py
+++ main.py
@@ -27,12 +27,31 @@ graphic driver and OpenGl works correctly.
 If this example runs on your machine, that means you're ready to explore the wide
 pythonocc world and run all the other examples.
 """
-
 from OCC.Display.SimpleGui import init_display
 from OCC.Core.BRepPrimAPI import BRepPrimAPI_MakeBox
+from OCC.Core.gp import gp_Lin2d, gp_Pnt2d, gp_Dir2d, gp_Circ2d, gp_Ax22d, gp_Ax3
+from OCC.Core.gp import *
+from OCC.Extend.ShapeFactory import make_edge2d
+from OCC.Extend.ShapeFactory import *
 
 display, start_display, add_menu, add_function_to_menu = init_display()
-my_box = BRepPrimAPI_MakeBox(10., 20., 30.).Shape()
 
-display.DisplayShape(my_box, update=True)
+ORIGIN = gp_Pnt(0, 0, 0)
+DIR_X = gp_Dir(1, 0, 0)
+DIR_Y = gp_Dir(0, 1, 0)
+DIR_Z = gp_Dir(0, 0, 1)
+LINE_X = gp_Lin(ORIGIN, DIR_X)
+LINE_Y = gp_Lin(ORIGIN, DIR_Y)
+LINE_Z = gp_Lin(ORIGIN, DIR_Z)
+display.DisplayShape(make_edge(LINE_X), update=True, color="RED")
+display.DisplayShape(make_edge(LINE_Y), update=True, color="GREEN")
+display.DisplayShape(make_edge(LINE_Z), update=True, color="BLUE")
+
+AX_XZ = gp_Ax2(gp_Pnt(0, 0, 0), gp_Dir(0, 1, 0), gp_Dir(0, 0, 1))
+AX_YZ = gp_Ax2(gp_Pnt(0, 0, 0), gp_Dir(1, 0, 0), gp_Dir(0, 0, 1))
+ci1 = gp_Circ(AX_XZ, 5)
+display.DisplayColoredShape(make_edge(ci1), update=True, color="GREEN" )
+ci2 = gp_Circ(AX_YZ, 5)
+display.DisplayColoredShape(make_edge(ci2), update=True, color="WHITE" )
+
 start_display()
