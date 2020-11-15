157c59536bcee3bef5a1f7a85cc14bd7f08958de
Mathew MacDougall <mmacd97@gmail.com>
2020-11-14 20:53:39 -0800

    minor cleanup and renaming

--- main.py
+++ main.py
@@ -20,55 +20,47 @@ def combine_faces(face1, face2):
 
     return common.Shape()
 
-def create_word(word1, word2):
+def combine_words(word1, word2):
     assert len(word1) == len(word2)
-    combined_letters = []
+
+    combined_faces = []
     for letter1, letter2 in zip(word1, word2):
         face1 = FaceFactory.create_letter(letter1)
         face2 = FaceFactory.create_letter(letter2)
         combined_letter = combine_faces(face1, face2)
-        combined_letters.append(combined_letter)
+        combined_faces.append(combined_letter)
 
     tf = gp_Trsf()
     p1 = ORIGIN
-    x = 0
-    new_letters = []
-    for l in combined_letters:
-        p2 = gp_Pnt(x, x, 0)
-        tf.SetTranslation(p1, p2)
-        new_letters.append(BRepBuilderAPI_Transform(l, tf).Shape())
-        x += 12
+    offset = 0
+    offset_letters = []
+    for l in combined_faces:
+        tf.SetTranslation(p1, gp_Pnt(offset, offset, 0))
+        offset_letters.append(BRepBuilderAPI_Transform(l, tf).Shape())
+        offset += 12
 
-    return new_letters
+    return offset_letters
 
 def main():
     display, start_display, add_menu, add_function_to_menu = init_display()
-    # Read keyboard input
-    word1 = 'IE'
-    word2 = 'FT'
 
-    letters = create_word(word1, word2)
-    for l in letters:
-        display.DisplayColoredShape(l, update=True, color="WHITE")
-
-    # Create faces
-    # face_1 = FaceFactory.create_letter(letter_1)
-    # face_2 = FaceFactory.create_letter(letter_2)
-
-    # display.DisplayShape(face_2, update=True, color="BLUE")
+    # face = FaceFactory.create_letter('C')
+    # display.DisplayShape(face, update=True, color="BLUE")
     # start_display()
     # return
 
-    # Create combined letter shape
-    # shape = combine_faces(face_1, face_2)
-
-    # Display final result
+    # Read keyboard input
+    word1 = 'IE'
+    word2 = 'FT'
 
     display.DisplayShape(make_edge(LINE_X), update=True, color="RED")
     display.DisplayShape(make_edge(LINE_Y), update=True, color="GREEN")
     display.DisplayShape(make_edge(LINE_Z), update=True, color="BLUE")
 
-    # display.DisplayColoredShape(shape, update=True, color="WHITE")
+    letters = combine_words(word1, word2)
+
+    for l in letters:
+        display.DisplayColoredShape(l, update=True, color="WHITE")
 
     start_display()
 
