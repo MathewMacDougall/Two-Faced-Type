d14ff80e9b66fe72824107aa43ec3696bb087dd3
Mathew MacDougall <mmacd97@gmail.com>
2020-11-18 22:11:10 -0800

    WIP: Working on supporting BezierCurves

--- face_factory.py
+++ face_factory.py
@@ -4,15 +4,35 @@ from OCC.Extend.ShapeFactory import make_face, make_edge
 from OCC.Core.BRep import BRep_Tool
 from PIL import Image
 import numpy as np
-from pathlib import Path
+import pathlib
 from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Fuse
 from functools import reduce
 from OCC.Extend.TopologyUtils import TopologyExplorer
-from svgpathtools import svg2paths, Line
+from svgpathtools import svg2paths, Line, CubicBezier, Path
 import logging
 logging.getLogger("PIL").setLevel(logging.WARNING)
 from constants import PL_XZ
 from shapely.geometry import Point, Polygon
+
+class PathHierarhcy():
+    def __init__(self, root_path=None, child_paths=None):
+        if child_paths is None:
+            child_paths = list()
+        self._root_path = root_path
+        self._child_paths = child_paths
+
+    def root_path(self):
+        assert self._root_path
+        return self._root_path
+
+    def child_paths(self):
+        return self._child_paths
+
+    def set_root_path(self, new_root_path):
+        self._root_path = new_root_path
+
+    def add_child_path(self, child_path):
+        self._child_paths.append(child_path)
 
 class FaceFactory():
     @classmethod
@@ -52,9 +72,10 @@ class FaceFactory():
         return paths
 
     @classmethod
-    def _lines_are_clockwise(cls, lines):
+    def _path_is_clockwise(cls, path):
+        assert isinstance(path, Path)
         # https://stackoverflow.com/a/1165943
-        vertices = [(line.start.real, line.start.imag) for line in lines]
+        vertices = [(path_component.start.real, path_component.start.imag) for path_component in path]
         curve_sum = sum((v2[0] - v1[0]) * (v2[1] + v1[1]) for v1, v2 in zip(vertices, vertices[1:] + [vertices[0]]))
         return curve_sum >= 0
 
@@ -74,7 +95,7 @@ class FaceFactory():
 
     @classmethod
     def create_from_image(cls, filepath, height_mm):
-        assert isinstance(filepath, Path)
+        assert isinstance(filepath, pathlib.Path)
         if not filepath.is_file():
             raise IOError("Unable to create Face from image file: {}. File does not exist".format(filepath))
 
@@ -88,15 +109,43 @@ class FaceFactory():
 
     @classmethod
     def _normalize_lines_clockwise(cls, paths):
+        assert isinstance(paths, list)
         normalized_paths = []
-        for lines in paths:
-            if cls._lines_are_clockwise(lines):
-                normalized_paths.append(lines)
-            else:
-                vertices = list(reversed([complex(line.start.real, line.start.imag) for line in lines]))
-                clockwise_lines = [Line(v1, v2) for v1, v2 in zip(vertices, vertices[1:] + [vertices[0]])]
-                normalized_paths.append(clockwise_lines)
+        for path in paths:
+            assert isinstance(path, Path)
+            path_is_clockwise = cls._path_is_clockwise(path)
+            if not path_is_clockwise:
+                return [path for path in paths]
+            clockwise_path = Path()
+            for line in reversed(path):
+                if isinstance(line, Line):
+                    new_start = line.end
+                    new_end = line.start
+                    line.start = new_start
+                    line.end = new_end
+                    clockwise_path.append(line)
+                elif isinstance(line, CubicBezier):
+                    new_start = line.end
+                    new_end = line.start
+                    new_control1 = line.control2
+                    new_control2 = line.control1
+                    line.start = new_start
+                    line.end = new_end
+                    line.control1 = new_control1
+                    line.control2 = new_control2
+                    clockwise_path.append(line)
+                else:
+                    raise RuntimeError("Invalid line type")
+            normalized_paths.append(clockwise_path)
         return normalized_paths
+        # for lines in paths:
+        #     if cls._path_is_clockwise(lines):
+        #         normalized_paths.append(lines)
+        #     else:
+        #         vertices = list(reversed([complex(line.start.real, line.start.imag) for line in lines]))
+        #         clockwise_lines = [Line(v1, v2) for v1, v2 in zip(vertices, vertices[1:] + [vertices[0]])]
+        #         normalized_paths.append(clockwise_lines)
+        # return normalized_paths
 
     @classmethod
     def _point_in_poly(cls, point, lines):
@@ -107,42 +156,90 @@ class FaceFactory():
 
     @classmethod
     def _create_path_hierarchy(cls, paths):
-        # list of lists, where the first element contains all subsequent ones
+        # Find path that is not contained by another
+        def get_root_path(all_paths):
+            for path in all_paths:
+                all_except_path = [p for p in all_paths if p != path]
+                path_vertices = [complex(line.start.real, line.start.imag) for line in path]
+                # TODO: This doesn't really account for Bezier curves, but is close enough for now
+                if not any([cls._point_in_poly(v, other_path) for other_path in all_except_path for v in path_vertices]):
+                    # No other path contains the current path
+                    return path
+            return None
+
+        def get_contained_paths(root_path, all_paths):
+            all_except_path = [p for p in all_paths if p != root_path]
+            contained_paths = []
+            for other_path in all_except_path:
+                # TODO: This doesn't really account for Bezier curves, but is close enough for now
+                other_path_vertices = [complex(line.start.real, line.start.imag) for line in other_path]
+                if any([cls._point_in_poly(v, root_path) for v in other_path_vertices]):
+                    contained_paths.append(other_path)
+            return contained_paths
+
+
         hierarchy = []
         paths_copy = [p for p in paths]
-        for p in paths:
-            ch = [p]
-            try:
-                paths_copy.remove(p)
-            except ValueError:
-                continue
-            other_paths_to_remove = []
-            for other_line in paths_copy:
-                other_path_vertices = [complex(line.start.real, line.start.imag) for line in other_line]
-                if any([cls._point_in_poly(v, p) for v in other_path_vertices]):
-                    # poly is contained at least partially by current path
-                    ch.append(other_line)
-                    other_paths_to_remove.append(other_line)
-            for temp_path in other_paths_to_remove:
-                try:
-                    paths_copy.remove(temp_path)
-                    print("removed path")
-                except ValueError:
-                    pass
-            hierarchy.append(ch)
-
+        while paths_copy:
+            root = get_root_path(paths_copy)
+            contained = get_contained_paths(root, paths_copy)
+            path_hierarchy = PathHierarhcy(root, contained)
+            hierarchy.append(path_hierarchy)
+            for cp in contained:
+                paths_copy.remove(cp)
+            paths_copy.remove(root)
         return hierarchy
+
+
+        # for path in paths:
+        #     path_hierarchy = PathHierarhcy()
+        #
+        #
+        #
+        #
+        #
+        #     ch = [path]
+        #     try:
+        #         paths_copy.remove(path)
+        #     except ValueError:
+        #         continue
+        #     other_paths_to_remove = []
+        #     for other_line in paths_copy:
+        #         other_path_vertices = [complex(line.start.real, line.start.imag) for line in other_line]
+        #         if any([cls._point_in_poly(v, path) for v in other_path_vertices]):
+        #             # poly is contained at least partially by current path
+        #             ch.append(other_line)
+        #             other_paths_to_remove.append(other_line)
+        #     for temp_path in other_paths_to_remove:
+        #         try:
+        #             paths_copy.remove(temp_path)
+        #             print("removed path")
+        #         except ValueError:
+        #             pass
+        #     hierarchy.append(ch)
+        #
+        # return hierarchy
 
     @classmethod
     def _create_from_svg(cls, filepath, height_mm):
-        assert isinstance(filepath, Path)
+        assert isinstance(filepath, pathlib.Path)
         if not filepath.is_file():
             raise IOError("Unable to create Face from image file: {}. File does not exist".format(filepath))
 
         paths, attributes = svg2paths(str(filepath))
-        continuous_paths = [cp for path in paths for cp in cls._get_contiguous_paths(path) ]
-        continuous_paths = cls._normalize_lines_clockwise(continuous_paths)
+        for path in paths:
+            print("type: {}     value: {}".format(type(path), path))
+
+        # TODO: is this needed? Are paths generally the single unit of continuity?
+        # continuous_paths = [cp for path in paths for cp in cls._get_contiguous_paths(path) ]
+        continuous_paths = cls._normalize_lines_clockwise(paths)
         continuous_paths = cls._create_path_hierarchy(continuous_paths)
+
+
+        # TODO: YOU ARE HERE
+        # Got hierarchy (probably) working. Need to convert paths/lines to edges
+
+
         faceMaker = BRepBuilderAPI_MakeFace(PL_XZ)
         # Right now can only handle a single "main" face with holes
         assert len(continuous_paths) < 2
@@ -185,7 +282,7 @@ class FaceFactory():
 
     @classmethod
     def _create_from_png(cls, filepath, height_mm):
-        assert isinstance(filepath, Path)
+        assert isinstance(filepath, pathlib.Path)
         if not filepath.is_file():
             raise IOError("Unable to create Face from image file: {}. File does not exist".format(filepath))
 
@@ -221,7 +318,7 @@ class FaceFactory():
 
         char = char.upper()
 
-        face_images_dir = Path(__file__).parent / "face_images"
+        face_images_dir = pathlib.Path(__file__).parent / "face_images"
         assert face_images_dir.is_dir()
 
         char_image_file = face_images_dir / "{}.png".format(char)
