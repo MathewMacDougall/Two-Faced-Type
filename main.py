04b057aec250d2fff742164bc4b0a18b7cbedf9a
Mathew MacDougall <mmacd97@gmail.com>
2020-12-02 20:20:08 -0800

    removed a bunch of commented out code and print statements

--- main.py
+++ main.py
@@ -165,21 +165,17 @@ def extrude_and_clamp(faces, vec, clamp, height_mm):
     return copy.deepcopy(result)
 
 def make_magic_solid(compound, containing_box, height_mm):
-    print("~~~ making magic solid")
     assert isinstance(compound, TopoDS_Compound)
     assert isinstance(containing_box, TopoDS_Solid)
     compound_ = copy.deepcopy(compound)
     containing_box_ = copy.deepcopy(containing_box)
 
     all_faces = get_list_from_compound(compound_, CompoundSequenceType.FACE)
-    print("all faces: {}".format(len(all_faces)))
     nonperp_faces = get_nonperp_faces(gp_Vec(0, 1, 0), all_faces)
-    print("nonperp faces: {}".format(len(nonperp_faces)))
     if not nonperp_faces:
         raise RuntimeError("AAAAAAAH")
     magic_compound = extrude_and_clamp(nonperp_faces, gp_Vec(0, 1, 0), containing_box_, height_mm)
     assert isinstance(magic_compound, TopoDS_Compound)
-    print("~~~ done making magic solid")
     return copy.deepcopy(magic_compound)
 
 
@@ -223,16 +219,13 @@ def get_mass(compound):
     compound_ = copy.deepcopy(compound)
 
     solids = get_list_from_compound(compound_, CompoundSequenceType.SOLID)
-    print("Found {} solids in the compound".format(len(solids)))
 
     total_mass = 0
     for solid in solids:
         props = GProp_GProps()
         BRepGProp.brepgprop_VolumeProperties(solid, props)
         mass = props.Mass()
-        print("partial mass: ", mass)
         total_mass += mass
-    print("total mass ", total_mass)
     return total_mass
 
 def remove_redundant_geometry(compound, face1, face2, height_mm):
@@ -249,7 +242,6 @@ def remove_redundant_geometry(compound, face1, face2, height_mm):
     # display.DisplayShape(face1_reference_solid)
 
     faces = get_list_from_compound(compound_, CompoundSequenceType.FACE)
-    print("total num faces: {}".format(len(faces)))
     cutting_extrusions = []
     for face in faces:
         gprop = BRepGProp_Face(face)
@@ -261,227 +253,33 @@ def remove_redundant_geometry(compound, face1, face2, height_mm):
         normal_extrusion = make_extrusion(face, height_mm, normal_vec_reversed)
         cutting_extrusions.append(normal_extrusion)
 
-    # NOTES:
-    # the display statement is causing foo to change (the magic solid for the temp piece)
-    # issue seems to be specifically in make_magic_solid function. Everything else works as expected / the optimized_solid_temp shape is ok
-    # IDEA: does an odd number of faces result in no extrusion? Do they cancel out?
-    # non-perp faces seem to be the same...
-    # containing box is the same
-    # magic-extrusions seem to be the same...
-
-    print("len cutting extrusions: {}".format(len(cutting_extrusions)))
     final_cutting_extrusions = []
     for index, cutting_extrusion in enumerate(cutting_extrusions):
         optimized_solid_temp = BRepAlgoAPI_Cut(compound_, cutting_extrusion).Shape()
 
         ost_mass = get_mass(optimized_solid_temp)
-        # print("ost mass: {}".format(ost_mass))
-        if index == 17:
-            # display.DisplayShape(optimized_solid_temp, color="CYAN", transparency=1.0) # This varies the problem
-            # display.DisplayShape(cutting_extrusion, color="BLACK", transparency=0.8)
-            print("\n\n\n{} index ost_mass: {}".format(index, ost_mass))
-            # continue
 
         if ost_mass < 1.001:
             # The face we're operating on is likely an entire face. Definitely can't remove it
-            # print("{} OST MASS SMALL".format(index))
             pass
         else:
             foo = make_magic_solid(optimized_solid_temp, bounding_box, height_mm)
-            # testvar33 = BRepAlgoAPI_Common(magic_extruded_faces, magic_clamp).Shape()
-            # foo = copy.deepcopy(testvar33)
             if foo:
                 diff = BRepAlgoAPI_Cut(face1_reference_solid, foo).Shape()
-                # display.DisplayShape(diff, color="RED", transparency=0.8)
                 diff_mass = get_mass(diff)
-                if index == 17:
-                    # display.DisplayShape(foo, transparency=0.5)
-                    # display.DisplayShape(magic_containing_box, color="BLUE", transparency=0.8)
-                    # display.DisplayShape(magic_extruded_faces, color="BLUE", transparency=0.8)
-                    # display.DisplayShape(magic_clamp, color="RED", transparency=1.0)
-                    # testvar = BRepAlgoAPI_Common(magic_extruded_faces, magic_clamp).Shape()
-                    # display.DisplayShape(testvar, transparency=0.5)
-                    # display.DisplayShape(magic_clamp, color="RED", transparency=0.8)
-                    # for me in magic_extrusions:
-                    #     display.DisplayShape(me, color=random_color(), transparency=0.8)
-                    # for npf in nonperp_faces:
-                    #     display.DisplayShape(npf, color="BLUE", transparency=0.8)
-                    # display.DisplayShape(optimized_solid_temp, color="BLUE", transparency=0.8)
-                    # display.DisplayShape(face1_reference_solid, color="BLUE", transparency=0.8)
-                    # display.DisplayShape(diff, color="RED", transparency=0.8)
-                    print("{} diff_mass: {}".format(index, diff_mass))
                 if diff_mass < 0.1:
                     final_cutting_extrusions.append(cutting_extrusion)
             else:
                 pass
-        if index == 17:
-            print("\n\n\n")
 
-    print("num final cutting extrusions: {}".format(len(final_cutting_extrusions)))
     final_geom = compound_
     for cut in final_cutting_extrusions:
         final_geom = BRepAlgoAPI_Cut(final_geom, cut).Shape()
 
     display.DisplayShape(final_geom, update=True, color="GREEN", transparency=0.9)
-    print("FACES REMOVED: {}".format(len(final_cutting_extrusions)))
     return copy.deepcopy(final_geom)
     # return None
 
-        # CHECK IF FACES STILL EXIST AS NEEDED
-
-        # WHY DOES THIS AFFECT BEHABIOR????
-        # display.DisplayShape(face, update=True, color="CYAN", transparency=0.8)
-        # if foo:
-        #     # display.DisplayShape(foo, color="CYAN", transparency=0.8)
-        #     diff = BRepAlgoAPI_Cut(face1_reference_solid, foo).Shape()
-        #     # display.DisplayShape(diff, color="RED", transparency=0.8)
-        #
-        #     diff_mass = get_mass(diff)
-        #     print("{}: MASS: {}".format(index, diff_mass))
-        #     if diff_mass > 10:
-        #         print("keeping diff {}".format(index))
-        #
-        #     else:
-        #         print("######### cutting diff {}".format(index))
-        #         optimized_solid = BRepAlgoAPI_Cut(optimized_solid, normal_extrusion).Shape()
-        #     # display.DisplayShape(optimized_solid, transparency=0.8)
-        # else:
-        #     print("keeping diff {}".format(index))
-            # optimized_solid = BRepAlgoAPI_Cut(optimized_solid, normal_extrusion).Shape()
-            # "GOOD" faces
-            # display.DisplayShape(face, update=True, color=random_color())
-
-
-    # display.DisplayShape(optimized_solid)
-    # break
-
-
-
-
-
-
-
-
-
-        #
-        # containing_box = solid_box(solid)
-        # # display.DisplayShape(containing_box, color="WHITE", transparency=0.9)
-        # negative_solid = BRepAlgoAPI_Cut(containing_box, solid).Shape()
-        # # display.DisplayShape(negative_solid, color="BLACK", transparency=0.8)
-        #
-        # face1_extruded = make_extrusion(face1, face_width(face2), gp_Vec(0, 1, 0))
-        # # display.DisplayShape(face1_extruded, color="BLUE", transparency=0.8)
-        # face1_extruded_trimmed = BRepAlgoAPI_Cut(face1_extruded, negative_solid).Shape()
-        # display.DisplayShape(face1_extruded_trimmed, color="BLUE", transparency=0.8)
-        #
-        # remainder = BRepAlgoAPI_Cut(face1_extruded_trimmed, optimized_solid_temp).Shape()
-        # # display.DisplayShape(remainder, transparency=0.5)
-        # from OCC.Core.TopoDS import TopoDS_Solid
-        # from OCC.Core.GProp import GProp_GProps
-        # from OCC.Core import BRepGProp
-        # props = GProp_GProps()
-        # BRepGProp.brepgprop_VolumeProperties(remainder, props)
-        # print("{}: MASS: {}".format(index, props.Mass()))
-
-    # display.DisplayShape(optimized_solid, transparency=0.8)
-
-    #
-    #
-    #
-    #
-    # from OCCUtils.Construct import project_edge_onto_plane
-    # from OCCUtils.Construct import make_edge as make_edge_foo
-    # from OCCUtils.edge import Edge
-    # from OCC.Core.GeomAdaptor import GeomAdaptor_Curve
-    # from OCC.Core.GeomProjLib import geomprojlib_ProjectOnPlane
-    # from OCC.Core.Prs3d import Prs3d_Projector
-    # from OCC.Core.HLRBRep import HLRBRep_Algo, HLRBRep_HLRToShape
-    # from OCC.Extend.TopologyUtils import HLRAlgo_Projector
-    # from OCC.Core.BRepAdaptor import BRepAdaptor_Curve, BRepAdaptor_HCurve
-    # from OCCUtils.edge import Edge
-    # from OCC.Core.Geom import Geom_Plane
-    # solid_edges = TopologyExplorer(solid).edges()
-    # face1edges = TopologyExplorer(face1).edges()
-    # # display.DisplayShape(face1edges)
-    # face2edges = TopologyExplorer(face2).edges()
-    # proj_edges = []
-    # for edge in face1edges:
-    #     e = Edge(edge)
-    #     pl = Geom_Plane(PL_XZ)
-    #     # display.DisplayShape(e, update=True, color="BLUE")
-    #     # display.DisplayShape(e.adaptor.Curve().Curve(), update=True, color="BLUE")
-    #     # adaptor = BRepAdaptor_Curve(e)
-    #     # display.DisplayShape(e.curve)
-    #     # proj_edges.append(make_edge())
-    #     # adaptor.
-    #
-    #     # baz = HLRAlgo_Projector(AX_XZ)
-    #     # # baz = HLRAlgo_Projector(AX_YZ)
-    #     # # baz.
-    #     # # projector = Prs3d_Projector(True, 100, proj_vector[0], proj_vector[1], proj_vector[2], view_point[0], view_point[1], view_point[2], vertical_direction_vector[0], vertical_direction_vector[1], vertical_direction_vector[2])
-    #     # # algo.Projector(projector.Projector())
-    #     # algo = HLRBRep_Algo()
-    #     # algo.Add(e)
-    #     # algo.Projector(baz)
-    #     # algo.Update()
-    #     # foobar = HLRBRep_HLRToShape(algo)
-    #     # projshape = foobar.VCompound()
-    #     # print(type(projshape))
-    #     # shapep = AIS_Shape(projshape).Shape()
-    #     # display.DisplayShape(projshape, update=True, color="BLUE")
-    #     # projshape = foobar.VCompound()
-    #     # projshape = foobar.OutLineHCompound()
-    #     # projshape = foobar.IsoLineHCompound()
-    #     # print(type(projshape))
-    #     # shapep = AIS_Shape(projshape).Shape()
-    #     # print(type(shapep))
-    #     # display.DisplayShape(projshape, update=True, color="BLACK")
-    #
-    #     # continue
-    #     # display.DisplayShape(e)
-    #     # proj_edges.append(project_edge_onto_plane(e, pl))
-    #     # from OCC.Core.GeomProjLib import geomprojlib_ProjectOnPlane
-    #     # proj = geomprojlib_ProjectOnPlane(e.adaptor.Curve().Curve(), pl, pl.Axis().Direction(), False)
-    #     proj = geomprojlib_ProjectOnPlane(e.adaptor.Curve().Curve(), pl, pl.Axis().Direction(), False)
-    #     print(e.adaptor.Curve().GetType())
-    #     print('projtype: ', type(proj))
-    #     projedge = Edge(make_edge(proj))
-    #     proj_edges.append(projedge)
-    #     print("projedge type: ", type(projedge))
-    #     test = GeomAdaptor_Curve(proj)
-    #     test2 = test.Trim(0, 1, 0.01)
-    #     display.DisplayShape(test2.Curve(), update=True, color="RED")
-    #
-    #     print('test type: ', type(test))
-    #     # try:
-    #     #     display.DisplayShape(projedge, update=True, color="RED")
-    #     # except Exception:
-    #     #     pass
-    #     # proj_edges.append(proj)
-    #     # Geom_Plane
-    #     # return make_edge(proj)
-    #
-    #     # proj = geomprojlib_ProjectOnPlane(edge.Curve(), PL_XZ, PL_XZ.Axis().Direction(), 1)
-    #     # proj_edges.append(project_edge_onto_plane(e, PL_XZ))
-    #
-    # # display.DisplayShape([proj_edges])
-    # # print("edge types")
-    # # test_edge = make_edge(gp_Pnt(0, 0, 100), gp_Pnt(100, 0, 100))
-    # # display.DisplayShape(test_edge, update=True, color="BLACK")
-    # # for pe in proj_edges:
-    # #     print(type(pe))
-    # #     # print(type(make_edge(pe)))
-    # #     try:
-    # #         display.DisplayShape(pe, update=True, color="RED")
-    # #     except Exception:
-    # #         pass
-    # #     print(pe.closest(test_edge))
-    #
-    #
-    #
-    #
-    #
-    # return None
 
 # Also, useful site to make svg letters: https://maketext.io/
 # My blessed documentation: https://old.opencascade.com/doc/occt-6.9.0/refman/html/class_geom2d___b_spline_curve.html#a521ec5263443aca0d5ec43cd3ed32ac6
@@ -503,171 +301,7 @@ def main(word1, word2, height_mm, output_dir):
     if new_geom:
         display.DisplayShape(new_geom, update=True, color="GREEN")
 
-
-
-    # for l in letters:
-    #     display.DisplayColoredShape(l, update=True, color="WHITE")
-
-    # def get_color():
-    #     while True:
-    #         for c in ["RED", "GREEN", "BLUE", "CYAN", "ORANGE", "YELLOW", "BLACK"]:
-    #             yield c
-    #
-    # for i in range(10):
-    #     print(get_color())
-    #
-    # letter = letters[0]
-    # print(type(letter))
-    # face1 = faces1[0]
-    # face2 = faces2[0]
-    # colors = get_color()
-    # from OCC.Extend.TopologyUtils import TopologyExplorer
-    # from OCC.Core import BRepTools
-    # from OCC.Core.BRepGProp import BRepGProp_Face
-    # from OCC.Core.GeomLProp import GeomLProp_SLProps
-    # # from OCC.Core.HLRTopoBRep import ToShape
-    # # from OCC.Core.HLRAlgo import HLRAlgo
-    # # from OCC.Core import HLRAlgo
-    # # from OCC.Core.HLRBRep import HLRBRep_HLRToShape
-    # from OCC.Core.Prs3d import Prs3d_Projector
-    # from OCC.Core.HLRBRep import HLRBRep_Algo, HLRBRep_HLRToShape
-    # from OCC.Extend.TopologyUtils import HLRAlgo_Projector
-    #
-    # # def face_center(face):
-    # #     face.bb
-    #
-    # # algo = HLRBRep_Algo()
-    # # algo.Add(letter)
-    # proj_vector = (1, 0, 0)
-    # view_point = (0, -100, 0)
-    # vertical_direction_vector = (0, 0, 1)
-    #
-    # # baz = HLRAlgo_Projector(AX_XZ)
-    # # baz = HLRAlgo_Projector(AX_YZ)
-    # # baz.
-    # # projector = Prs3d_Projector(True, 100, proj_vector[0], proj_vector[1], proj_vector[2], view_point[0], view_point[1], view_point[2], vertical_direction_vector[0], vertical_direction_vector[1], vertical_direction_vector[2])
-    # # algo.Projector(projector.Projector())
-    # # algo.Projector(baz)
-    # # algo.Update()
-    # # foobar = HLRBRep_HLRToShape(algo)
-    # # projshape = foobar.VCompound()
-    # # projshape = foobar.VCompound()
-    # # projshape = foobar.OutLineHCompound()
-    # # projshape = foobar.IsoLineHCompound()
-    # # print(type(projshape))
-    # # shapep = AIS_Shape(projshape).Shape()
-    # # print(type(shapep))
-    # # display.DisplayShape(projshape, update=True, color="BLACK")
-    # # print(type(foobar))
-    # # shape = None
-    # # foobar.HCompound(shape)
-    # # print(shape)
-    # # proj_shape = AIS_Shape(foobar.HCompound())
-    # # display.DisplayShape(letter, update=True, color="BLACK")
-    # from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Common
-    # from OCC.Core.BRepExtrema import BRepExtrema_DistShapeShape
-    # # from OCC.Core.TopoDS import m
-    #
-    # from OCC.Extend.TopologyUtils import TopologyExplorer
-    # projshape_edges = TopologyExplorer(letter).edges()
-    # # BRepAlgoAPI_Common
-    # face1_edges = TopologyExplorer(letter).edges()
-    # pse = None
-    # pse2 = None
-    # for i, e in enumerate(projshape_edges):
-    #     if i == 10:
-    #         pse = e
-    #     if i == 11:
-    #         pse2 = e
-    # fe = None
-    # fe2 = None
-    # for i, e in enumerate(face1_edges):
-    #     if i == 4:
-    #         fe = e
-    #     if i == 5:
-    #         fe2 = e
-    # print("types: {}, {}".format(fe, pse))
-    # print("types: {}, {}".format(fe2, pse2))
-    #
-    # # display.DisplayShape(pse, update=True, color="BLACK")
-    # # display.DisplayShape(fe, update=True, color="WHITE")
-    # print("INFO")
-    # print('orientation ', fe.Orientation())
-    # print('null ', fe.IsNull())
-    # foo = make_edge(gp_Pnt(0, 0, 0), gp_Pnt(10, 10, 10))
-    # print(type(foo))
-    # # display.DisplayShape(foo, update=True, color="CYAN")
-    # dss = BRepExtrema_DistShapeShape(fe, fe2)
-    # print(dss.Value())
-    # # d = fe.closest(pse)
-    # # dss.LoadS1(fe)
-    # # dss.LoadS2(pse)
-    # # dss.Perform()
-    # # for fe in face1_edges:
-    # #     for pse in projshape_edges:
-    # #         print("types: {}, {}".format(fe, pse))
-    # #         # display.DisplayShape([fe, pse])
-    # #         display.DisplayShape(pse, update=True, color="BLACK")
-    # #         display.DisplayShape(fe, update=True, color="WHITE")
-    # #         dss = BRepExtrema_DistShapeShape()
-    # #         # d = fe.closest(pse)
-    # #         dss.LoadS1(fe)
-    # #         dss.LoadS2(pse)
-    # #         dss.Perform()
-    # #         break
-    # #         # assert dss.IsDone()
-    # #         # print(dss.Value())
-    # #     break
-    #
-    #
-    #
-    #
-    #
-    #
-    # # exp = TopologyExplorer(letter)
-    # # # for edge in exp.edges():
-    # # #     display.DisplayShape(edge, update=True, color=next(colors))
-    # # for face in exp.faces():
-    # #     display.DisplayShape(face, update=True, color=next(colors))
-    # #
-    # #     foo = BRepGProp_Face(face)
-    # #     normal_point = gp_Pnt(0, 0, 0)
-    # #     normal_vec = gp_Vec(0, 0, 0)
-    # #     # TODO: how to get middle of face with UV mapping?
-    # #     foo.Normal(0, 0, normal_point, normal_vec)
-    # #     # display.DisplayShape(normal_point, update=True, color="BLACK")
-    # #     normal_vec.Reverse()
-    # #     # normal_extrusion = make_extrusion(face, 2*height_mm, normal_vec)
-    # #     # display.DisplayShape(normal_extrusion, update=True, color="BLACK")
-    # #     # letter = BRepAlgoAPI_Cut(letter, normal_extrusion).Shape()
-    # #
-    # #     normal_extrusion = make_extrusion(face, 2*height_mm, normal_vec)
-    # #     # display.DisplayShape(normal_extrusion, update=True, color="BLACK")
-    # #     letter = BRepAlgoAPI_Cut(letter, normal_extrusion).Shape()
-    # #
-    # #     algo = HLRBRep_Algo()
-    # #     algo.Add(letter)
-    # #     projector = Prs3d_Projector(False, 0, 0, 0, 5, 0, 0, 1, 0, 1, 0)
-    # #     algo.Projector(projector.Projector())
-    # #     algo.Update()
-    # #     foobar = HLRBRep_HLRToShape(algo)
-    # #     print(type(foobar))
-    # #     shape = None
-    # #     foobar.HCompound(shape)
-    # #     print(shape)
-    # #     proj_shape = AIS_Shape(foobar.HCompound())
-    # #     # display.DisplayShape(foobar.HCompound, update=True, color="CYAN")
-    # #
-    # #     # print(normal_point)
-    # #     # display.DisplayShape(v, update=True, color="BLACK")
-    # #     # print(normal_vec)
-    # #
-    # #     # print(face.normalAt(0, 0))
-    # #     # face
-    # #     break
-    # # display.DisplayShape(letter, update=True, color="BLACK")
-    #
-    # # save_to_stl(letters, output_dir)
+    # save_to_stl(letters, output_dir)
 
     start_display()
 
@@ -680,6 +314,5 @@ if __name__ == '__main__':
     parser.add_argument('--height', metavar='height_mm', type=float, help="The height of the characters, in mm",
                         required=True)
     args = parser.parse_args()
-    print(args)
 
     main(args.words[0], args.words[1], args.height, args.output_dir)
