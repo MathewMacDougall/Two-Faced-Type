abb27f01a8a6a2c378694fd9079ecc11351f310b
Mathew MacDougall <mmacd97@gmail.com>
2020-12-04 19:59:02 -0800

    Really stuck on Fusion bug. Seems that if a face has any point that's tangent to the direction of extrusion, that will cause Fusions later-on to fail. Tried several ways of doing this, including with FuzzyValues, but no luck.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from OCC.Core.AIS import AIS_Shape
 import copy
 from OCC.Core.BRep import BRep_Builder
+from OCC.Core.GC import GC_MakeArcOfCircle, GC_MakeSegment
+from OCC.Core.ShapeAnalysis import ShapeAnalysis_Surface
 from OCC.Core.ShapeExtend import ShapeExtend_Explorer
 from enum import Enum
 from OCC.Core.TopTools import TopTools_HSequenceOfShape
@@ -19,7 +21,7 @@ from OCC.Display.SimpleGui import init_display
 from OCC.Extend.ShapeFactory import make_extrusion, make_edge, make_face
 from face_factory import FaceFactory
 from OCC.Core.BRepAlgoAPI import BRepAlgoAPI_Common, BRepAlgoAPI_Cut, BRepAlgoAPI_Fuse, BRepAlgoAPI_Check, BRepAlgoAPI_Section, BRepAlgoAPI_Splitter
-from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_Transform
+from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_Transform, BRepBuilderAPI_MakeEdge, BRepBuilderAPI_MakeWire
 from OCC.Core.gp import gp_Trsf, gp_Ax1, gp_Vec, gp_Pnt
 from constants import *
 import math
@@ -130,7 +132,7 @@ def dot(v1, v2):
     assert isinstance(result, float)
     return result
 
-def get_nonperp_faces(faces, vec):
+def get_nonperp_faces(faces, vec, disp=False):
     assert isinstance(vec, gp_Vec)
     assert isinstance(faces, list)
 
@@ -140,10 +142,41 @@ def get_nonperp_faces(faces, vec):
         gprop = BRepGProp_Face(face)
         normal_point = gp_Pnt(0, 0, 0)
         normal_vec = gp_Vec(0, 0, 0)
+        foo_face = Face(face)
+        # get uv coord of middle
+        u_min, u_max, v_min, v_max = foo_face.domain()
+        # u_mid = (u_min + u_max) / 2.
+        # v_mid = (v_min + v_max) / 2.
+        u_mid = u_min + (u_max - u_min) / 100.
+        v_mid = v_min + (v_max - v_min) / 100.
+        # print("{}, {}, {}, {}, {}, {}".format(u_min, u_mid, u_max, v_min, v_mid, v_max))
+
+        # sas = ShapeAnalysis_Surface(foo_face.surface)
+        # uv = sas.ValueOfUV(gp_pnt_middle, 1.)
+        # uv_middle = uv.Coord()
+        # print(uv_middle.X())
         # TODO: how to get middle of face with UV mapping?
-        gprop.Normal(0, 0, normal_point, normal_vec)
-        if abs(dot(vec, normal_vec)) > 0.01:
+        # UV coord has to be on face to work
+        gprop.Normal(u_mid, v_mid, normal_point, normal_vec)
+        # gprop.Normal(0, 0, normal_point, normal_vec)
+        # print("normal pt: {}".format(normal_point))
+        # print("normal vec: {}".format(normal_vec))
+
+        # if disp:
+        #     c = random_color()
+        #     display.DisplayShape(face, color=c, transparency=0.8)
+        #     display.DisplayShape(normal_point, color=c)
+        # display.DisplayShape(normal_point, color="BLACK")
+        if abs(dot(vec, normal_vec)) != 0:
+            # print("good")
             nonperp_faces.append(face)
+            # if disp or True:
+            #     # print("drawing")
+            #     c = random_color()
+            #     display.DisplayShape(face, color=c, transparency=0.6)
+            #     display.DisplayShape(normal_point, color=c)
+        else:
+            print("DOT PRODUCT VALUE: {}".format(abs(dot(vec, normal_vec))))
     return copy.deepcopy(nonperp_faces)
 
 def extrude_and_clamp(faces, vec, clamp, height_mm):
@@ -159,23 +192,100 @@ def extrude_and_clamp(faces, vec, clamp, height_mm):
     extrusions = []
     for face in faces_:
         assert isinstance(face, TopoDS_Face)
-        extrusions.append(make_extrusion(face, 2 * height_mm, vec_))
+        ext = make_extrusion(face, 2 * height_mm, vec_)
+        # display.DisplayShape(ext, color=random_color(), transparency=0.6)
+        extrusions.append(ext)
 
     # for e in extrusions:
         # display.DisplayShape(e, color=random_color(), transparency=0.8)
 
     # for e in extrusions:
     #     display.DisplayShape(e, transparency=0.7, color=random_color())
-    builder = BOPAlgo_Builder()
-    for extrusion in extrusions:
-        builder.AddArgument(extrusion)
-        # display.DisplayShape(extrusion, color=random_color(), transparency=0.8)
-    builder.SetRunParallel(True)
-    builder.Perform()
-    if builder.HasErrors():
-        raise AssertionError("Failed to combine extrusions. AlgoBuilder failed with error: ", builder.DumpErrorsToString())
-    extruded_faces = builder.Shape()
-    print("build shape type: {}".format(type(extruded_faces)))
+
+    # Tyring alternative builder algos
+    # builder = BOPAlgo_Builder()
+    # for extrusion in extrusions:
+    #     builder.AddArgument(extrusion)
+    #     # display.DisplayShape(extrusion, color=random_color(), transparency=0.8)
+    # builder.SetRunParallel(False)
+    # builder.Perform()
+    # if builder.HasErrors():
+    #     # for extrusion in extrusions:
+    #     #     display.DisplayShape(extrusion, color=random_color(), transparency=0.8)
+    #     for f in faces:
+    #         display.DisplayShape(f, color=random_color(), transparency=0.6)
+    #     print("ABOUT TO FAIL")
+    #     start_display()
+    #     raise AssertionError("Failed to combine extrusions. AlgoBuilder failed with error: ", builder.DumpErrorsToString())
+    # extruded_faces = builder.Shape()
+
+
+    # result = copy.deepcopy(clamp_)
+    # newlist = []
+    # for e in extrusions:
+    #     newlist.append(copy.deepcopy(BRepAlgoAPI_Common(result, e).Shape()))
+
+    # https://github.com/tpaviot/pythonocc-core/issues/641
+    from OCC.Core.TopTools import TopTools_ListOfShape
+    f2 = BRepAlgoAPI_Fuse()
+    L1 = TopTools_ListOfShape()
+    L1.Append(extrusions[0])
+    L2 = TopTools_ListOfShape()
+    L2.Append(extrusions[1])
+    f2.SetArguments(L2)
+    f2.SetTools(L1)
+    f2.SetFuzzyValue(0.1)
+    realresult = f2.Build()
+
+    display.DisplayShape(realresult.Shape())
+
+
+    # real_result = newlist[0]
+    # real_result = extrusions[0]
+    # display.DisplayShape(real_result)
+    # for rr in extrusions[1:2]:
+    #     fuser = BRepAlgoAPI_Fuse(real_result, rr)
+    #     fuser.SetFuzzyValue(1.1)
+    #     foo = fuser.Build()
+    #     print("foo null?: {}".format(foo is None))
+    #     display.DisplayShape(real_result, color="RED", transparency=0.7)
+    start_display()
+    # for qq in newlist:
+    #     display.DisplayShape(qq)
+
+    # builder = BOPAlgo_Builder()
+    # for qq in newlist:
+    #     builder.AddArgument(qq)
+    #     # display.DisplayShape(extrusion, color=random_color(), transparency=0.8)
+    # builder.SetRunParallel(False)
+    # builder.Perform()
+    # if builder.HasErrors():
+    #     # for extrusion in extrusions:
+    #     #     display.DisplayShape(extrusion, color=random_color(), transparency=0.8)
+    #     # for f in faces:
+    #     #     display.DisplayShape(f, color=random_color(), transparency=0.6)
+    #     print("ABOUT TO FAIL AGAIN")
+    #     start_display()
+    #     raise AssertionError("Failed to combine extrusions. AlgoBuilder failed with error: ", builder.DumpErrorsToString())
+    # extruded_faces = builder.Shape()
+    # display.DisplayShape(extruded_faces)
+
+
+    # display.DisplayShape(result)
+    start_display()
+    # BRepAlgoAPI_Common(clamp_, extruded_faces).Shape()
+
+    # extruded_faces = extrusions[0]
+    # for e in extrusions[1:]:
+    #     extruded_faces = BRepAlgoAPI_Fuse(extruded_faces, e).Shape()
+    print("build shape type: {}".format(type(result)))
+
+
+
+
+
+
+
     # display.DisplayShape(extruded_faces, color=random_color(), transparency=0.8)
     # display.DisplayShape(clamp, color=random_color(), transparency=0.95)
 
@@ -194,16 +304,29 @@ def extrude_and_clamp(faces, vec, clamp, height_mm):
     #     display.DisplayShape(extruded_faces, transparency=0.8)
     #     display.DisplayShape(clamp_, color="WHITE", transparency=0.8)
     assert isinstance(result, TopoDS_Compound)
+
+    # for f in faces:
+    #     display.DisplayShape(f, color=random_color(), transparency=0.6)
+    # start_display()
     return copy.deepcopy(result)
 
-def project_and_clamp(compound, vec, containing_box, height_mm):
+def project_and_clamp(compound, vec, containing_box, height_mm, disp=False):
     assert isinstance(compound, TopoDS_Compound)
     assert isinstance(containing_box, TopoDS_Solid)
     compound_ = copy.deepcopy(compound)
     containing_box_ = copy.deepcopy(containing_box)
 
+    # if disp:
+    #     display.DisplayShape(compound_)
+
     all_faces = get_list_from_compound(compound_, CompoundSequenceType.FACE)
-    nonperp_faces = get_nonperp_faces(all_faces, vec)
+    # if disp:
+    #     for f in all_faces:
+    #         display.DisplayShape(f)
+    nonperp_faces = get_nonperp_faces(all_faces, vec, disp)
+    # if disp:
+    #     for f in nonperp_faces:
+    #         display.DisplayShape(f, color=random_color(), transparency=0.8)
     if not nonperp_faces:
         raise RuntimeError("No nonperp faces. This probably shouldn't happen.")
     magic_compound = extrude_and_clamp(nonperp_faces, vec, containing_box_, height_mm)
@@ -259,7 +382,7 @@ def remove_redundant_geometry(shapes, height_mm):
     return [_remove_redundant_geometry_helper(shape, height_mm) for shape in shapes]
 
 def face_is_valid(temp_cut_compound_, vec_, bounding_box_, reference_solid, height_mm):
-    temp_projected_compound = project_and_clamp(temp_cut_compound_, vec_, bounding_box_, height_mm)
+    temp_projected_compound = project_and_clamp(temp_cut_compound_, vec_, bounding_box_, height_mm, disp=True)
     diff = BRepAlgoAPI_Cut(reference_solid, temp_projected_compound).Shape()
     diff_mass = get_mass(diff)
     return diff_mass < 0.1
@@ -295,7 +418,9 @@ def _remove_redundant_geometry_helper(compound, height_mm):
 
     final_cutting_extrusions = []
     for index, cutting_extrusion in enumerate(cutting_extrusions):
-        print("cutting extrusion... {}".format(index / len(cutting_extrusions)))
+        if index != 12:
+            continue
+        print("{}: cutting extrusion... {}".format(index, index / len(cutting_extrusions)))
         temp_cut_compound = BRepAlgoAPI_Cut(compound_, cutting_extrusion).Shape()
 
         temp_cut_compound_mass = get_mass(temp_cut_compound)
@@ -306,6 +431,7 @@ def _remove_redundant_geometry_helper(compound, height_mm):
             # from a face that covers the entire object, and We likely shouldn't remove it.
             # face1_valid = face_is_valid(temp_cut_compound, face1_vec, bounding_box, face1_reference_solid, height_mm)
             face1_valid = True
+            # display.DisplayShape(cutting_extrusion, color="WHITE")
             face2_valid = face_is_valid(temp_cut_compound, face2_vec, bounding_box, face2_reference_solid, height_mm)
             # face2_valid = True
             if face1_valid and face2_valid:
@@ -333,19 +459,63 @@ def main(word1, word2, height_mm, output_dir):
     display.DisplayShape(make_edge(LINE_Z), update=True, color="BLUE")
 
     letters, faces1, faces2 = combine_words(word1, word2, height_mm)
+    letters = remove_redundant_geometry(letters, height_mm)
     try:
         letters = remove_redundant_geometry(letters, height_mm)
-    except:
-        print("ERROR")
+    except AssertionError as e:
+        # print(e)
+        # if e
+        # if e
+        # print("ERROR")
         pass
-    # letters = offset_shapes(letters, height_mm)
+    letters = offset_shapes(letters, height_mm)
 
-    for letter in letters:
-        if letter:
-            display.DisplayShape(letter)
+    # for letter in letters:
+    #     if letter:
+    #         display.DisplayShape(letter)
 
     # save_to_stl(letters, output_dir)
 
+    start_display()
+
+def test_bad_fusion():
+    height = 70
+    width = 50
+    thickness = 30
+
+    print("creating bottle")
+    # The points we'll use to create the profile of the bottle's body
+    aPnt1 = gp_Pnt(0, 0, 0)
+    aPnt2 = gp_Pnt(50, 0, 0)
+    aPnt3 = gp_Pnt(60, 0, 10)
+    aPnt4 = gp_Pnt(70, 0, 20)
+    aPnt5 = gp_Pnt(70, 0, 50)
+    aPnt6 = gp_Pnt(60, 0, 60)
+    aPnt7 = gp_Pnt(50, 0, 70)
+    aPnt8 = gp_Pnt(0, 0, 70)
+    # aPnt4 = gp_Pnt(width / 2.0, -thickness / 4.0, 0)
+    # aPnt5 = gp_Pnt(width / 2.0, 0, 0)
+
+    aSegment1 = GC_MakeSegment(aPnt1, aPnt2)
+    aArcOfCircle = GC_MakeArcOfCircle(aPnt2, aPnt3, aPnt4)
+    aSegment2 = GC_MakeSegment(aPnt4, aPnt5)
+    aArcOfCircle2 = GC_MakeArcOfCircle(aPnt5, aPnt6, aPnt7)
+    aSegment3 = GC_MakeSegment(aPnt7, aPnt8)
+
+    # Could also construct the line edges directly using the points instead of the resulting line
+    aEdge1 = BRepBuilderAPI_MakeEdge(aSegment1.Value())
+    aEdge2 = BRepBuilderAPI_MakeEdge(aArcOfCircle.Value())
+    aEdge3 = BRepBuilderAPI_MakeEdge(aSegment2.Value())
+    aEdge4 = BRepBuilderAPI_MakeEdge(aArcOfCircle2.Value())
+    aEdge5 = BRepBuilderAPI_MakeEdge(aSegment3.Value())
+
+    # Create a wire out of the edges
+    aWire = BRepBuilderAPI_MakeWire(aEdge1.Edge(), aEdge2.Edge(), aEdge3.Edge(), aEdge4.Edge(), aEdge5.Edge())
+
+    display.DisplayShape(make_edge(LINE_X), update=True, color="RED")
+    display.DisplayShape(make_edge(LINE_Y), update=True, color="GREEN")
+    display.DisplayShape(make_edge(LINE_Z), update=True, color="BLUE")
+    display.DisplayShape(aWire.Shape())
     start_display()
 
 
@@ -358,4 +528,6 @@ if __name__ == '__main__':
                         required=True)
     args = parser.parse_args()
 
-    main(args.words[0], args.words[1], args.height, args.output_dir)
+    # main(args.words[0], args.words[1], args.height, args.output_dir)
+    test_bad_fusion()
+
